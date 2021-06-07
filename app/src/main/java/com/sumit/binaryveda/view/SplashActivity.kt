69c70fca1a52b22fba676a25407445f0254b4b28
Sumit Kotal <48079369+Sumit-Kotal19@users.noreply.github.com>
2021-06-07 16:12:25 +0530

    bugfix gradle prop

--- app/src/main/java/com/sumit/binaryveda/view/SplashActivity.kt
+++ app/src/main/java/com/sumit/binaryveda/view/SplashActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.sumit.apps10x.R
+import com.sumit.binaryveda.R
 
 class SplashActivity : AppCompatActivity() {
     val SPLASH_DELAY: Long = 3000 //Time delay to remove splash screen
